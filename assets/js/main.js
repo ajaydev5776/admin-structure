3c9e449c2525316fff3ffeeb141e640b72bf2a5b
Lipika Agarwal <87571584+LipikaAgarwal@users.noreply.github.com>
2024-02-21 20:07:52 +0530

    code-push

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,29 +11,7 @@ switchcancle.addEventListener('click', () =>{
 
  
     
-// let  enableinput = document.getElementById('enableinput');
-// let  disableinput = document.getElementById('disableinput');
-// disableinput.onclick = function(){
-//     var form = document.getElementById('userdetails');
-//     var inputs = form.getElementsByTagName('input');
 
-//     for (var i = 0; i < inputs.length; i++) {
-//         if (inputs[i].type !== 'button') {
-//             inputs[i].disabled = true;
-//         }
-//     }
-//     enableinput.classList.remove('active')
-// }
-// enableinput.onclick = function(){
-//     var form = document.getElementById('userdetails');
-//     var inputs = form.getElementsByTagName('input');
-//     for (var i = 0; i < inputs.length; i++) {
-//         if (inputs[i].type !== 'button') {
-//             inputs[i].disabled = false;
-//         }
-//     }
-//     enableinput.classList.add('active')
-// }
 
 // In your Javascript (external .js resource or <script> tag)
     $(document).ready(function() {
@@ -85,55 +63,26 @@ function togglePasswordVisibility() {
   });
 
 
-// $(document).ready(function() {
-//     let myTable = $('#example').DataTable({
-//         columnDefs: [{
-//             orderable: false,
-//             className: 'select-checkbox',
-//             targets: 0,
-//         }],
-//         select: {
-//             style: 'os', // 'single', 'multi', 'os', 'multi+shift'
-//             selector: 'td:first-child',
-//         },
-//         order: [
-//             [1, 'asc'],
-//         ],
-//     });
+let  enableinput = document.getElementById('enableinput');
+let  disableinput = document.getElementById('disableinput');
+disableinput.onclick = function(){
+    var form = document.getElementById('userdetails');
+    var inputs = form.getElementsByTagName('input');
 
-//     $('#MyTableCheckAllButton').click(function() {
-//         if (myTable.rows({
-//                 selected: true
-//             }).count() > 0) {
-//             myTable.rows().deselect();
-//             return;
-//         }
-
-//         myTable.rows().select();
-//     });
-
-//     myTable.on('select deselect', function(e, dt, type, indexes) {
-//         if (type === 'row') {
-//             // We may use dt instead of myTable to have the freshest data.
-//             if (dt.rows().count() === dt.rows({
-//                     selected: true
-//                 }).count()) {
-//                 // Deselect all items button.
-//                 $('#MyTableCheckAllButton i').attr('class', 'far fa-check-square');
-//                 return;
-//             }
-
-//             if (dt.rows({
-//                     selected: true
-//                 }).count() === 0) {
-//                 // Select all items button.
-//                 $('#MyTableCheckAllButton i').attr('class', 'far fa-square');
-//                 return;
-//             }
-
-//             // Deselect some items button.
-//             $('#MyTableCheckAllButton i').attr('class', 'far fa-minus-square');
-//         }
-//     });
-// });
- 
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type !== 'button') {
+            inputs[i].disabled = true;
+        }
+    }
+    enableinput.classList.remove('active')
+}
+enableinput.onclick = function(){
+    var form = document.getElementById('userdetails');
+    var inputs = form.getElementsByTagName('input');
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type !== 'button') {
+            inputs[i].disabled = false;
+        }
+    }
+    enableinput.classList.add('active')
+}
